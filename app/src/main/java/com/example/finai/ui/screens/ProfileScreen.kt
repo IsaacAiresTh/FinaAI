e64438c6f159f55a915faa50124540b543b8687b
caioosm <matossousacaio.cms@gmail.com>
2025-10-19 16:35:00 -0300

    feat(screen): Tela que sera usada para perfil do usuario

---
+++ app/src/main/java/com/example/finai/ui/screens/ProfileScreen.kt
@@ -0,0 +1,22 @@
+package com.example.finai.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ProfileScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF2C2A2A)),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Profile Screen", color = Color.White)
+    }
+}
